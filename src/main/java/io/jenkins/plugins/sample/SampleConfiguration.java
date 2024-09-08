b4a1097250e5fe92fc0ba874e5a1930adeec8238
98yejin <o0o2211915@gmail.com>
2024-09-08 17:50:02 +0900

    change configs

--- src/main/java/io/jenkins/plugins/sample/SampleConfiguration.java
+++ src/main/java/io/jenkins/plugins/sample/SampleConfiguration.java
@@ -9,98 +9,88 @@ import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.bind.JavaScriptMethod;
 
-/**
- * Example of Jenkins global configuration.
- */
 @Extension
 public class SampleConfiguration extends GlobalConfiguration {
 
-    /** @return the singleton instance */
-    public static SampleConfiguration get() {
-        return ExtensionList.lookupSingleton(SampleConfiguration.class);
-    }
-
-    private String dbConnectionInfo;
-    private int dashboardPort;
-    private int dataRetentionPeriod;
-    private String jobNameRegex;
+    private boolean showUptime;
+    private boolean showOnlineAgents;
+    private boolean showOfflineAgents;
+    private boolean showTotalAgents;
+    private boolean showMemoryUsage;
+    private boolean showCpuUsage;
+    private boolean showDiskUsage;
 
     public SampleConfiguration() {
-        // When Jenkins is restarted, load any saved configuration from disk.
         load();
     }
 
     @DataBoundSetter
-    public void setDbConnectionInfo(String dbConnectionInfo) {
-        this.dbConnectionInfo = dbConnectionInfo;
+    public void setShowUptime(boolean showUptime) {
+        this.showUptime = showUptime;
         save();
     }
 
-    public String getDbConnectionInfo() {
-        return dbConnectionInfo;
-    }
-
-    @JavaScriptMethod
-    public FormValidation doTestConnection(@QueryParameter("dbConnectionInfo") final String dbConnectionInfo) {
-        try {
-            // 예시: 데이터베이스 연결 테스트 로직 (JDBC 사용 예)
-            if (dbConnectionInfo == null || dbConnectionInfo.isEmpty()) {
-                return FormValidation.error("Database connection information is empty");
-            }
-
-            // Connection connection = DriverManager.getConnection(dbConnectionInfo);
-            // connection.close();
-
-            return FormValidation.ok("Connection successful!");
-        } catch (Exception e) {
-            return FormValidation.error("Connection failed: " + e.getMessage());
-        }
+    public boolean isShowUptime() {
+        return showUptime;
     }
 
     @DataBoundSetter
-    public void setDashboardPort(int dashboardPort) {
-        this.dashboardPort = dashboardPort;
+    public void setShowOnlineAgents(boolean showOnlineAgents) {
+        this.showOnlineAgents = showOnlineAgents;
         save();
     }
 
-    public int getDashboardPort() {
-        return dashboardPort;
+    public boolean isShowOnlineAgents() {
+        return showOnlineAgents;
     }
 
     @DataBoundSetter
-    public void setDataRetentionPeriod(int dataRetentionPeriod) {
-        this.dataRetentionPeriod = dataRetentionPeriod;
+    public void setShowOfflineAgents(boolean showOfflineAgents) {
+        this.showOfflineAgents = showOfflineAgents;
         save();
     }
 
-    public int getDataRetentionPeriod() {
-        return dataRetentionPeriod;
+    public boolean isShowOfflineAgents() {
+        return showOfflineAgents;
     }
 
     @DataBoundSetter
-    public void setJobNameRegex(String jobNameRegex) {
-        this.jobNameRegex = jobNameRegex;
+    public void setShowTotalAgents(boolean showTotalAgents) {
+        this.showTotalAgents = showTotalAgents;
         save();
     }
 
-    public String getJobNameRegex() {
-        return jobNameRegex;
+    public boolean isShowTotalAgents() {
+        return showTotalAgents;
     }
 
-    //    public String getLabel() {
-    //        return label;
-    //    }
-    //
-    //    @DataBoundSetter
-    //    public void setLabel(String label) {
-    //        this.label = label;
-    //        save();
-    //    }
+    @DataBoundSetter
+    public void setShowMemoryUsage(boolean showMemoryUsage) {
+        this.showMemoryUsage = showMemoryUsage;
+        save();
+    }
 
-    public FormValidation doCheckLabel(@QueryParameter String value) {
-        if (StringUtils.isEmpty(value)) {
-            return FormValidation.warning("Please specify a label.");
-        }
-        return FormValidation.ok();
+    public boolean isShowMemoryUsage() {
+        return showMemoryUsage;
+    }
+
+    @DataBoundSetter
+    public void setShowCpuUsage(boolean showCpuUsage) {
+        this.showCpuUsage = showCpuUsage;
+        save();
+    }
+
+    public boolean isShowCpuUsage() {
+        return showCpuUsage;
+    }
+
+    @DataBoundSetter
+    public void setShowDiskUsage(boolean showDiskUsage) {
+        this.showDiskUsage = showDiskUsage;
+        save();
+    }
+
+    public boolean isShowDiskUsage() {
+        return showDiskUsage;
     }
 }
